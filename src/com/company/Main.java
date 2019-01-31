3b79659b3ad5d1e6b754f89bb568535a4545f06f
jukegbu1 <jukegbu1@gmail.com>
2019-01-31 13:51:22 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for(int i=10; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
